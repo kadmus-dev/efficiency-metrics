48a33e4f17224ba6ec554231ae862ca5547ddf3b
Nikita Alutis <nikalutis@yandex.ru>
2022-03-20 10:47:20 +0300

    merge conflicts resolved

--- app/main.py
+++ app/main.py
@@ -5,8 +5,7 @@ sys.path.append(os.path.join(os.getcwd(), 'pages'))
 
 import streamlit as st
 from multi_page import MultiPage
-from pages import cocomo, third_step_plots, sqlite, other_page, project_start
-from pages import mean_satisfaction_coefficient
+from pages import cocomo, sqlite, project_start
 from pages import third_step_project
 from pages import third_step_staff
 
@@ -20,9 +19,7 @@ def main():
 
     # Add all your applications (pages) here
     app.add_page("Cocomo metric", cocomo.app)
-    app.add_page("Other page header", other_page.app)
     app.add_page("Project Start", project_start.app)
-    app.add_page("Result plots", third_step_plots.app)
     app.add_page("SQLite", sqlite.app)
     app.add_page("3 этап (проекты)", third_step_project.app)
     app.add_page("3 этап (сотрудники)", third_step_staff.app)
